e29f88a26be7c8a3f565525a74c5206a65820451
Ted Smith <tssmith327@gmail.com>
2016-04-16 22:43:26 -0500

    Release 1 do the thing werewolve questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,4 @@
+puts "What is your name?"
+puts "How old are you, where were you born?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Would you like to enroll in the company's insurance?"
