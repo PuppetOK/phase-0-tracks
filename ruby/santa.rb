04a6d0c16b76cd0c5d241c836741337960a851a9
Ted Smith <tssmith327@gmail.com>
2016-05-01 21:55:21 -0500

    added the reindeer rank method to the class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,8 +14,14 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
   end
-  
+  def reindeer_ranking
+    reindeer_ranking = []
+    reindeer = "Rudolph,Dasher,Dancer,Prancer,Vixen,Comet,Cupid,Donner,Blitzen"
+    reindeer_ranking << reindeer.split(",")
+  end
+    
 end
 test1 = Santa.new("white", "white")
 p test1.speak
 p test1.eat_milk_and_cookies("snickerdoodle")
+puts test1.reindeer_ranking
