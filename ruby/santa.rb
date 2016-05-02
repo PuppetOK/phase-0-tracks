b0bced2dee988ed315f0fb4fe7720279d0594bff
Ted Smith <tssmith327@gmail.com>
2016-05-01 21:44:04 -0500

    added the gender and ethnicity in the initialize method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,18 +1,21 @@
 class Santa
-	def speak
-		puts "Ho, ho, ho! Haaapy holidays!"
-	end
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    puts "Initializing Santa instance..."
+    puts "This Santas gender is: #{gender}"
+    puts "This Santa is: #{ethnicity} ethnicity"
+  end
+  
+  def speak
+    puts "Ho, ho, ho! Haaapy holidays!"
+  end
 
-	def eat_milk_and_cookies(cookie_type)
-		puts "That was a good #{cookie_type}!"
-	end
-
-	def initialize 
-		puts "Initializing Santa instance..."
-	end
-
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+  
 end
-test1 = Santa.new
+test1 = Santa.new("white", "white")
 p test1.speak
 p test1.eat_milk_and_cookies("snickerdoodle")
-p test1.initialize
