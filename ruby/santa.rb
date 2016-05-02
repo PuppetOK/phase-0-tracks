d4fcad9c9d9a96038b336d9ea893beea64a73672
Ted Smith <tssmith327@gmail.com>
2016-05-02 02:57:52 -0500

    added driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,8 @@
 class Santa
+  attr_reader :age, :ethnicity
+  attr_accessor :gender
+  
+  
   def initialize(gender, ethnicity)
     @gender = gender
     @ethnicity = ethnicity
@@ -18,17 +22,46 @@ class Santa
   
   def reindeer_ranking
     reindeer_ranking = []
-    reindeer = "Rudolph,Dasher,Dancer,Prancer,Vixen,Comet,Cupid,Donner,Blitzen"
-    reindeer_ranking << reindeer.split(",")
+    reindeer = "Rudolph Dasher Dancer Prancer Vixen Comet Cupid Donner Blitzen"
+    reindeer_ranking << reindeer.split
   end
   
   def age(age)
     @age = age
   end
+  
+  def celebrate_birthday
+    @age += 1
+  end
+  
+  def get_mad_at(reindeer_name)
+    reindeer_ranking.insert(-1, reindeer_name)
+  end
+    
     
 end
 santa = Santa.new("male", "white")
 p santa.speak
 p santa.eat_milk_and_cookies("snickerdoodle")
 puts santa.reindeer_ranking
-puts santa.age(12)
+puts santa.age(30)
+puts santa.celebrate_birthday
+puts santa.get_mad_at("Vixen")
+
+
+
+
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+multi_santa = []
+
+10.times do
+  multi_santa << Santa.new(example_genders.sample, example_ethnicities.sample)
+end
+
+multi_santa.each {|santa| puts "#{santa} is #{santa.ethnicity} ethnicity, and a #{santa.gender}."}
