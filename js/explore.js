06bc46214a625f12385db5ec7888b91fa443abf8
Ted Smith <tssmith327@gmail.com>
2016-05-07 10:46:25 -0500

    made the JS directory and the explore file with the reverse method

---
+++ js/explore.js
@@ -0,0 +1,13 @@
+var reversedString;
+
+
+
+function reverse(reversedString){
+	
+	console.log(reversedString.split("").reverse().join(""));
+}
+
+reverse("There is One");
+reverse("HELLO");
+reverse("Hello World!")
+reverse("This is a string reversed.")
