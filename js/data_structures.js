3000833763d86e116b66859a89c993fcb24e3ab1
Ted Smith <tssmith327@gmail.com>
2016-05-09 13:17:30 -0500

    added the car object

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,3 +13,16 @@ for (var i = 0; i < colors.length; i++){
 	horseColors[names[i]] = colors[i];
 }
 console.log(horseColors);
+
+function Car(make, model, condition, price){
+	this.make = make;
+	this.model = model;
+	this.condition = condition;
+	this.price = price;
+	console.log("New car stats!:", make, model, "New or Used?:",condition, "price:", price);
+	this.rev_engine = function() {console.log("VROOOOOOOOM")};
+}
+var carOne = new Car("Honda", "Civic", "used", 12000);
+carOne.rev_engine();
+var carTwo = new Car("BMW,", "M5,", "New", 40000);
+carTwo.rev_engine();
