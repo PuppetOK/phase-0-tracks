cc0cbc2ffa68c39425f01530125bc24a62b28d7f
Ted Smith <tssmith327@gmail.com>
2016-05-09 12:19:15 -0500

    added the colors and name arrays

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "Grey", "Orange", "white"]
+var names = ["Ed", "Horse Face", "Henry", "Bob"]
+
+colors.push("Blackish-blue");
+names.push("Mr.Long-legs");
+
+console.log(colors);
+console.log(names);
