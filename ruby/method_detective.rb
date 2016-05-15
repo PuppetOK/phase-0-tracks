d86ef531d75890b58c8180936d322c1ea88347a1
Ted Smith <tssmith327@gmail.com>
2016-05-15 10:33:39 -0500

    fixed merge issue

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,6 +4,7 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
+<<<<<<< HEAD
 # "iNvEsTiGaTiOn".<???>
 # => “InVeStIgAtIoN”
 
@@ -32,8 +33,43 @@
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+=======
+“iNvEsTiGaTiOn”.gsub /..?/, &:capitalize
+# => “InVeStIgAtIoN”
 
+ “zom”.insert(2,'0')
+# => “zoom”
+
+ “enhance”.center(20)
+#=> “    enhance    ”
+
+“Stop! You’re under arrest!”.upcase
+# => “STOP! YOU’RE UNDER ARREST!”
+
+ “the usual”.insert(-1, " suspects")
+#=> “the usual suspects”
+
+“ suspects”.insert(0, "the usual")
+#=> “the usual suspects”
+
+ “The case of the disappearing last letter”.chop
+# => “The case of the disappearing last lette”
+
+ “The mystery of the missing first letter”.slice(1..-1)
+# => “he mystery of the missing first letter”
+
+ “Elementary  ,    my   dear      Watson!”.squeeze
+# => “Elementary, my dear Watson!”
+
+“z”.ord
+>>>>>>> added the methods for method_detective file
+# => 122 
+# ascii decimal binary hex conversion
+
+<<<<<<< HEAD
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4
+=======
+“How many times does the letter ‘a’ appear in this string?”.count('a')
+#=> 4
+>>>>>>> added the methods for method_detective file
