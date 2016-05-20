9b6ae64c47d35356b4fbf605da532df74cf38a21
Ted Smith <tssmith327@gmail.com>
2016-05-20 05:11:57 -0500

    messed around with the kitten maker file

--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -9,36 +9,33 @@ require 'faker'
 # create SQLite3 database
 db = SQLite3::Database.new("kittens.db")
 db.results_as_hash = true
-
 # learn about fancy string delimiters
 create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS kittens(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    age INT
-  )
+	CREATE TABLE IF NOT EXISTS kittens(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		age INT
+	)
 SQL
-
 # create a kittens table (if it's not there already)
+
 db.execute(create_table_cmd)
 
 # add a test kitten
-# db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
+
+#db.execute("INSERT INTO Kittens (name, age) VALUES ('Bob', 10)")
 
 # add LOOOOTS of kittens!
-# so. many. kittens. 
-#KittenExplosion
 def create_kitten(db, name, age)
-  db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
+	db.execute("INSERT INTO kittens (name, age) VALUES (?,?)", [name, age])
 end
 
-10000.times do
-  create_kitten(db, Faker::Name.name, 0)
+10000.times do 
+	create_kitten(db,Faker::Name.name,0) 
 end
-
 # explore ORM by retrieving data
 # kittens = db.execute("SELECT * FROM kittens")
 # kittens.each do |kitten|
-#  puts "#{kitten['name']} is #{kitten['age']}"
+# 	puts "#{kitten['name']} is #{kitten['age']}"
 # end
 
